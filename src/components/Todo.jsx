310b49686a931fe1d95b97f37411ccb70af850ca
avg403 <adarshvg403.gmail.com>
2024-01-05 13:18:06 +0530

    Implemented functionality to add new todos and mark completed todos as done

---
+++ src/components/Todo.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit } from "@fortawesome/free-regular-svg-icons";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+
+const Todo = ({ task, toggleComplete }) => {
+  return (
+    <div className="Todo">
+      <p
+        onClick={() => toggleComplete(task.id)}
+        className={`${task.completed ? "completed" : ""}`}
+      >
+        {task.task}
+      </p>
+      <div>
+        <FontAwesomeIcon icon={faEdit} />
+        <FontAwesomeIcon icon={faTrash} />
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
